714b7305600fd11edb2049e269130a073e294f99
Ricardo Perdomo <hndgd1980@gmail.com>
2019-09-29 16:40:53 -0500

    adjusted routes

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // API and HTML routes
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+require("./app/routes/apiRoutes.js")(app);
+require("./app/routes/htmlRoutes.js")(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
